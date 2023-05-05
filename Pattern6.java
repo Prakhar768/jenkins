9bca3709c20b96a35408b4264c7a8ba992e691a4
Prakhar768 <83468838+Prakhar768@users.noreply.github.com>
2023-05-05 16:38:56 +0530

    new commit

---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+package day4_nestedLoop_Strings;
+/*  ABCDEEDCBA
+ *  ABCD  DCBA
+ *  ABC    CBA
+ *  AB      BA
+ *  A        A
+ * 
+ */
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
